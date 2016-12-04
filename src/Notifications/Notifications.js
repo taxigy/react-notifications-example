805838c7fff3aee904d446157700ebc83a21b687
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-12-04 01:46:11 -0500

    better looking notifications

--- src/Notifications/Notifications.js
+++ src/Notifications/Notifications.js
@@ -4,11 +4,10 @@ import styles from './Notifications.scss';
 
 function destroy(notification = {}) {
   const {
-    category = 'info',
     onClose
   } = notification;
 
-  if (category === 'info' && onClose instanceof Function) {
+  if (onClose instanceof Function) {
     setTimeout(onClose, 90 * 1000);
   }
 };
@@ -20,28 +19,38 @@ export default function (props) {
 
   return (
     <div className={styles.notifications}>
-      {_.map(notifications, (notification, index) => destroy(notification) || (
-        <div
-          key={index}
-          className={styles.notification}>
+      {_.map(_.slice(notifications, 0, 5), (notification, index) => {
+        const {
+          category = 'info'
+        } = notification;
+
+        if (category === 'info') {
+          destroy(notification);
+        }
+
+        return (
           <div
-            className={styles.notification__caption}
-            onClick={notification.onClose}>
-            <div className={styles.notification__header}>
-              {notification.header}
-            </div>
+            key={index}
+            className={styles.notification}>
             <div
-              className={styles.notification__closeButton}>
-              <i className="fa fa-times" />
+              className={[styles.notification__caption, styles[`notification__caption--${category}`]].join(' ')}
+              onClick={notification.onClose}>
+              <div className={styles.notification__header}>
+                {notification.header}
+              </div>
+              <div
+                className={styles.notification__closeButton}>
+                <i className="fa fa-times" />
+              </div>
             </div>
+            {notification.body && (
+              <div className={styles.notification__body}>
+                {notification.body}
+              </div>
+            )}
           </div>
-          {notification.body && (
-            <div className={styles.notification__body}>
-              {notification.body}
-            </div>
-          )}
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
